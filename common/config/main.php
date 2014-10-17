1982f143ae1857e3c1b1a3b080f07046524d1b81
Andrushin Anton <andrushin.anton@gmail.com>
2014-10-17 22:53:10 +0500

    Added redis support

--- common/config/main.php
+++ common/config/main.php
@@ -3,7 +3,20 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\redis\Cache',
+            'redis' => [
+                'hostname' => 'localhost',
+                'port' => 6379,
+                'database' => 0,
+            ],
+        ],
+        'session' => [
+            'class' => 'yii\redis\Session',
+            'redis' => [
+                'hostname' => 'localhost',
+                'port' => 6379,
+                'database' => 0,
+            ],
         ],
     ],
 ];
