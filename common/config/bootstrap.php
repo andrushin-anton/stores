e32ee37a005505be08c1c227b2ccce48529160cb
anton <fixiuss@yandex.ru>
2014-10-17 21:26:33 +0500

    Init

---
+++ common/config/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+Yii::setAlias('common', dirname(__DIR__));
+Yii::setAlias('acoolakids', dirname(dirname(__DIR__)) . '/acoolakids');
+Yii::setAlias('m.acoolakids', dirname(dirname(__DIR__)) . '/m.acoolakids');
+Yii::setAlias('conceptclub', dirname(dirname(__DIR__)) . '/conceptclub');
+Yii::setAlias('m.conceptclub', dirname(dirname(__DIR__)) . '/m.conceptclub');
+Yii::setAlias('infinitylingerie', dirname(dirname(__DIR__)) . '/infinitylingerie');
+Yii::setAlias('m.infinitylingerie', dirname(dirname(__DIR__)) . '/m.infinitylingerie');
+Yii::setAlias('admin.acoolakids', dirname(dirname(__DIR__)) . '/admin.acoolakids');
+Yii::setAlias('admin.conceptclub', dirname(dirname(__DIR__)) . '/admin.conceptclub');
+Yii::setAlias('admin.infinitylingerie', dirname(dirname(__DIR__)) . '/admin.infinitylingerie');
+Yii::setAlias('manager', dirname(dirname(__DIR__)) . '/manager');
+Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
